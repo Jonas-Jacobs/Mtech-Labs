002172ffdb58ef5be1806a12ff55698c22a276a8
Jonas Jacobs <Jonas.jacobs6475@stu.metc.edu>
2021-08-27 16:16:47 -0600

    2.2 Functions

--- Class Work/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
+++ Class Work/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
@@ -3,13 +3,24 @@
  
  Write a new introduction function called `introduction`. It should take two `String` parameters, `name` and `home`, and one `Int` parameter, `age`. The function should print a brief introduction. I.e. if "Mary," "California," and 32 were passed into the function, it might print "Mary, 32, is from California." Call the function and observe the printout.
  */
-
+func intro(name : String, home: String, age: Int){
+    print("\(name), \(age), is from \(home).")
+}
+intro(name: "Jonas", home: "Vashon", age: 17)
 
 //:  Write a function called `almostAddition` that takes two `Int` arguments. The first argument should not require an argument label. The function should add the two arguments together, subtract 2, then print the result. Call the function and observe the printout.
-
-
+var Num = 0
+func almostAddition(_ int: Int, _ int2: Int){
+  Num = int + int2
+    print(Num)
+}
+almostAddition(3, 3)
 //:  Write a function called `multiply` that takes two `Double` arguments. The function should multiply the two arguments and print the result. The first argument should not require a label, and the second argument should have an external label, "by", that differs from the internal label. Call the function and observe the printout.
 
+func multiply(_ num1: Double, by num2: Double) {
+    print(num1 * num2)
+}
+multiply(3, by: 4)
 
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Progress Updates](@next)
--- Class Work/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Class Work/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,10 +3,19 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
-
-
+func greeting(_ name: String ) -> String {
+    print("Hello \(name), how are you today?")
+    
+    return name
+}
+greeting("Jonas")
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
+func multiplication(_ num1: Int, _ num2: Int){
+    print(num1 * num2)
+    return num1 ; num2
+    
+}
+multiplication(3, 4)
 
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
